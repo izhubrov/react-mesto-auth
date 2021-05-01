21722658194e0b17cb35d55a539767764543b6c4
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-05-01 18:05:41 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer page__container">
-      <p className="footer__author">&copy; 2021 Mesto Russia</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-const apiSettings = {
-  baseUrl: "https://mesto.nomoreparties.co/v1",
-  groupId: "cohort-22",
-  token: "a835ebe3-157b-4140-922b-a5f49168fcac",
-};
-
-export { apiSettings };
